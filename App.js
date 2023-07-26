78876e99175c654d1d43b60a0b164b2ecdc4edaa
Aditya Sorte <68781943+Adityasorte@users.noreply.github.com>
2023-07-26 11:46:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, { text: newTask, completed: false }]);
+      setNewTask('');
+    }
+  };
+
+  const handleEditTask = (index, newText) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].text = newText;
+    setTasks(updatedTasks);
+  };
+
+  const handleToggleCompletion = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
+  };
+
+  const handleDeleteTask = (index) => {
+    const updatedTasks = tasks.filter((task, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="app">
+      <h1>To-Do List</h1>
+      <div className="task-input">
+        <input
+          type="text"
+          placeholder="Enter a new task"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={handleAddTask}>Add</button>
+      </div>
+      <ul className="task-list">
+        {tasks.map((task, index) => (
+          <li key={index} className={task.completed ? 'completed' : ''}>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => handleToggleCompletion(index)}
+            />
+            <input
+              type="text"
+              value={task.text}
+              onChange={(e) => handleEditTask(index, e.target.value)}
+            />
+            <button onClick={() => handleDeleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
